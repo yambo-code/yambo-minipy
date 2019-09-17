4f9a03089b18eef5aa190adc5f8884d91165b6f3
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2019-09-17 12:29:16 +0200

    Test script

--- yambo_test.py
+++ yambo_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import numpy as np
+import sys
 from pathlib import Path,os
+from subprocess import check_output, CalledProcessError, STDOUT
 
 """
 Simple python script to test yambo
@@ -56,6 +58,17 @@ def read_test_list():
 #    for test in flist: print(test)
     return flist
 
+def getstatusoutput(cmd):
+    try:
+        data = check_output(cmd, shell=True, universal_newlines=True, stderr=STDOUT)
+        status = 0
+    except CalledProcessError as ex:
+        data = ex.output
+        status = ex.returncode
+    if data[-1:] == '\n':
+        data = data[:-1]
+    return status, data
+
 
 def os_system(command, verbose=True, failure_handling='exit', fake=False):
     """
@@ -77,7 +90,7 @@ def os_system(command, verbose=True, failure_handling='exit', fake=False):
     ================  ========================================================
     """
     if verbose:
-        print('Running operating system command\n   %s' % command)
+        print('\nRunning operating system command:  %s' % command)
     if fake:
         return 0, 'testing "%s"' % command
 
@@ -87,7 +100,7 @@ def os_system(command, verbose=True, failure_handling='exit', fake=False):
         failure = result.close()
     else:
         # Unix/Linux/Mac:
-        failure, output = commands.getstatusoutput(command)
+        failure, output = getstatusoutput(command)
 
     if failure:
         msg = 'Failure when running operating system command'\
@@ -109,17 +122,15 @@ def os_system(command, verbose=True, failure_handling='exit', fake=False):
 
 
 
-def run(options='', inputfile=''):
+def run(program='',options='', inputfile=''):
     """Run program, store output on logfile."""
     # the logfile is always opened in the constructor so
     # we can safely append here
+
     if inputfile == '':
         logfile="logfile"
     else:
         logfile=inputfile+".log"
-
-
-    program=yambo_bin.joinpath(yambo_file).name
 
     vfile = open(logfile, 'a')
     vfile.write('\n#### Test: %s' % (program+"_"+inputfile))
@@ -133,8 +144,8 @@ def run(options='', inputfile=''):
     failure, output = os_system(cmd, failure_handling='silent')
     if failure:
         vfile = open(logfile, 'a')
-        msg = 'ERROR in %s: execution failure arose from the ' \
-              'command\n  %s\n\n%s\n\n' % (scriptfilename,cmd,output)
+        msg = 'ERROR in execution failure arose from the ' \
+              'command\n  %s\n\n%s\n\n' % (cmd,output)
         vfile.write(msg)
         vfile.close()
         print(msg)
@@ -166,12 +177,42 @@ print("\n * * * Yambo python tests * * * \n\n")
 
 check_code()
 
-tlist=read_test_list()
+test_list=read_test_list()
 
-for test in tlist:
+for test in test_list:
+    # ************ Running test **************
     print("Running test: "+test.name+"...", end='')
-    test_ok=True
-    test_ok=run(inputfile=test.name)
-#    test_ok=test_ok and compare_test(test)
-    print(test_ok)
 
+    inputfile=inputs_dir.joinpath(test.name).absolute().as_posix()
+
+    if "ypp" in test.name:
+        # ********* Running ypp **************
+        program  =yambo_bin.joinpath(ypp_file).absolute().as_posix()
+    else:
+        # ********** Running Yambo ***********
+        program  =yambo_bin.joinpath(yambo_file).absolute().as_posix()
+
+
+    # ******** Setup flags for the test ******
+    my_file = inputs_dir.joinpath(test.name+".flags")
+    
+    options=''
+    if my_file.is_file():
+        flag_file=open(my_file,"r")
+        flag=flag_file.read()
+        options=options+"-O "+flag
+        flag_file.close()
+
+    options=options+"-J "+test.name
+
+    print(program)
+    print(options)
+#    test_ok=run(program=program,options=options,inputfile=inputfile)
+
+    test_ok=True
+    if(test_ok):
+    # *********** Compare the results ********
+        print(test_ok)
+    else:
+        print("KO!")
+#
